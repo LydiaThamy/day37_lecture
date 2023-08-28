ae295a221316b39e2fa297364e0539e65b4c212e
LydiaThamy <lydiathamy@gmail.com>
2023-08-28 14:27:32 +0800

    first commit

---
+++ server/day37_lecture/src/main/java/vttp/day37_lecture/Day37LectureApplication.java
@@ -0,0 +1,13 @@
+package vttp.day37_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37LectureApplication.class, args);
+	}
+
+}
---
+++ server/day37_lecture/src/test/java/vttp/day37_lecture/Day37LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.day37_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
